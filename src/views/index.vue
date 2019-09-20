fbf738ef6db27d360429dbaff8256fd4923030a6
Jiang888* <jiangguanqun001@deppon.com>
2019-09-20 20:36:54 +0800

    add.

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,9 +1,20 @@
 <template>
     <div id="index">
         <Card>
-            <p slot="title">Iview Table Drag Function</p>
+            <p slot="title">Iview Table Drag Function -1</p>
             <Table 
                 :columns="columns" 
+                stripe
+                border
+                size="small"
+                :data="data">
+            </Table>
+        </Card><br/>
+
+        <Card>
+            <p slot="title">Iview Table Drag Function -2</p>
+            <Table 
+                :columns="columns_2" 
                 stripe
                 border
                 size="small"
@@ -27,12 +38,18 @@
                     {title: 'Time',key: 'time'},
                     {title: 'Remarks',key: 'remarks'},
                 ],
+
+                columns_2: [
+                    {title: 'Name',key: 'name',fixed: 'left'},
+                    {title: 'Age',key: 'age'},
+                    {title: 'Address',key: 'address'},
+                    {title: 'Emaill',key: 'emaill'},
+                    {title: 'Phone',key: 'phone'},
+                    {title: 'Time',key: 'time'},
+                    {title: 'Remarks',key: 'remarks'},
+                ],
+
                 data: [
-                    {name: '小明',age: 18,address: '上海市青浦区金字芋路2千弄500室',emaill: '3131244726@qq.com',phone:'123456789',time:'2019-09-06 10:35 00',remarks:'无...',},
-                    {name: '小明',age: 18,address: '上海市青浦区金字芋路2千弄500室',emaill: '3131244726@qq.com',phone:'123456789',time:'2019-09-06 10:35 00',remarks:'无...',},
-                    {name: '小明',age: 18,address: '上海市青浦区金字芋路2千弄500室',emaill: '3131244726@qq.com',phone:'123456789',time:'2019-09-06 10:35 00',remarks:'无...',},
-                    {name: '小明',age: 18,address: '上海市青浦区金字芋路2千弄500室',emaill: '3131244726@qq.com',phone:'123456789',time:'2019-09-06 10:35 00',remarks:'无...',},
-                    {name: '小明',age: 18,address: '上海市青浦区金字芋路2千弄500室',emaill: '3131244726@qq.com',phone:'123456789',time:'2019-09-06 10:35 00',remarks:'无...',},
                     {name: '小明',age: 18,address: '上海市青浦区金字芋路2千弄500室',emaill: '3131244726@qq.com',phone:'123456789',time:'2019-09-06 10:35 00',remarks:'无...',},
                     {name: '小明',age: 18,address: '上海市青浦区金字芋路2千弄500室',emaill: '3131244726@qq.com',phone:'123456789',time:'2019-09-06 10:35 00',remarks:'无...',},
                     {name: '小明',age: 18,address: '上海市青浦区金字芋路2千弄500室',emaill: '3131244726@qq.com',phone:'123456789',time:'2019-09-06 10:35 00',remarks:'无...',},
@@ -44,6 +61,13 @@
         created() {
             // 初始化设置表格默认配置项
             this.columns.forEach(v => {
+                v['align'] = 'center';
+                v['tooltip'] = true;
+                v['minWidth'] = 100;
+                v['sortable'] = true;
+            });
+
+            this.columns_2.forEach(v => {
                 v['align'] = 'center';
                 v['tooltip'] = true;
                 v['minWidth'] = 100;
